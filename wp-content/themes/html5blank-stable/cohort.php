80e2e3964e041f823172c06131ec3461e0850ec5
geekwise_veronica_chavez <veroch03@gmail.com>
2016-05-17 18:09:35 +0000

    cohort two in place but not displaying

--- wp-content/themes/html5blank-stable/cohort.php
+++ wp-content/themes/html5blank-stable/cohort.php
@@ -2,92 +2,71 @@
 <?php get_header('cohort'); ?>
  <main class="cohort_main">
   
-            <?php query_posts('post_type=geekwise_cohort'); ?>
-                    
-         <?php while (have_posts()) : the_post(); ?>    
+           
         <section class="cohort_top">
-       
-           <h1 class="cohort_question"><?php the_field('question'); ?></h1>
-           <p class="paragraph"><?php the_field('para'); ?></p>
-            <p class="paragragh"><?php the_field('para2'); ?></p>
+             <?php query_posts('post_type=geekwise_cohort'); ?> 
+           <?php while (have_posts()) : the_post(); ?> 
+        <?php the_content(); ?>
+          <?php endwhile; ?>
+          
+           <!--<h1 class="cohort_question"></h1>-->
+           <!--<p class="paragraph"></p>-->
+           <!-- <p class="paragragh"></p>-->
             
           
-                <span class="goals"><?php the_field('goal_start'); ?> </span>
-                <ol>
-                <li class="info_goals"><?php the_field('one'); ?></li>
-                <li class="info_goals"><?php the_field('two'); ?></li>
-                <li  class="info_goals"><?php the_field('three'); ?></li>
-                <li  class="info_goals"><?php the_field('four'); ?></li>
-                <li  class="info_goals"><?php the_field('five'); ?></li>
-                <span class="goals"><?php the_field('goal_end'); ?></span>
+           <!--     <span class="goals"><?php the_field('goal_start'); ?> </span>-->
+           <!--     <ol>-->
+           <!--     <li class="info_goals"><?php the_field('one'); ?></li>-->
+           <!--     <li class="info_goals"><?php the_field('two'); ?></li>-->
+           <!--     <li  class="info_goals"><?php the_field('three'); ?></li>-->
+           <!--     <li  class="info_goals"><?php the_field('four'); ?></li>-->
+           <!--     <li  class="info_goals"><?php the_field('five'); ?></li>-->
+           <!--     <span class="goals"><?php the_field('goal_end'); ?></span>-->
                
-            </ol>
+           <!-- </ol>-->
         
        </section>
-            <?php endwhile; ?>
+          
     </main>
     
     <section class="cohort_members">
-       <h1 id="members_text">Meet the Geekwise Academy Cohort Classes!</h1>
      
+           
+       <h1 id="members_text">Meet the Geekwise Academy Cohort Classes!</h1>
+       
            <div class="dropdown_bar">
                <label for="cohort_data" class="class_members" >
                    <i class="fa fa-angle-down down_angle"></i> Cohort Class 2 (July 2015 - January 2016) </label>
                   <input type="checkbox" id="cohort_data">
                <div class="group_bar first_class">
-                
-        
-              <div class="class2">
-                <?php while (have_posts()) : the_post(); ?> 
+        <?php query_posts('post_type=geekwise_cohort'); ?>        
+       <?php while (have_posts()) : the_post(); ?> 
+              <div class="class2_crud">
+               
                  <div class="photos_cohort"><?php the_post_thumbnail(); ?></div>
-                  <h1 class="name"><?php the_field('name'); ?></h1>
-                  <p class="story"><?php the_field('story'); ?> </p>
-                    <?php endwhile; ?>
+                  <h1 class="name_crud"><?php the_field('name'); ?></h1>
+                  <p class="story"><?php the_field('description'); ?> </p>
+                  
               </div>
-            
+             <?php endwhile; ?>
            </div>
-       
+          </div> 
+        
        <div class="dropdown_bar">
         <label for="cohort_data2" class="class_members class_mem2" >
-                   <i class="fa fa-angle-down down_angle"></i>  </label>
+                   <i class="fa fa-angle-down down_angle"></i>Cohort Class 1 (January 2015 - July 2015)</label>
                   <input type="checkbox" id="cohort_data2">
                   <div class="group_bar">
-                      <div class="class2">
-                          <img src="http://i.imgur.com/2S08cR0.jpg" alt="name" class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p>
+                   
+         <?php query_posts('post_type=geekwise_cohort_two'); ?>                       
+       <?php while (have_posts()) : the_post(); ?> 
+                      <div class="class2_crud">
+                       
+                          <div class="photos_cohort"><?php the_post_thumbnail(); ?></div>
+                          <h1 class="name"><?php the_field('name2'); ?></h1>
+                          <p class="story"><?php the_field('description2'); ?></p>
                       </div>
-                      <div class="class2">
-                          <img src="http://i.imgur.com/7ydkI3R.jpg" alt="name"  class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p>
-                      </div>
-                      <div class="class2">
-                          <img src="http://i.imgur.com/9Z83jBM.jpg" alt="name"  class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p>
-                      </div>
-                      <div class="class2">
-                          <img src="http://i.imgur.com/s1feR8v.jpg" alt=""  class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p>
-                      </div>
-                      <div class="class2">
-                          <img src="http://i.imgur.com/qdejOcf.jpg" alt="name"  class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p></div>
-                      <div class="class2">
-                          <img src="http://i.imgur.com/LIH0jPA.jpg" alt="name"  class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p> </div>
-                      <div class="class2">
-                          <img src="http://i.imgur.com/BzHjQDD.jpg" alt="name"  class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p> </div>
-                      <div class="class2">
-                          <img src="http://i.imgur.com/XtyYWsw.jpg" alt="name"  class="photos_cohort">
-                          <h1 class="name"></h1>
-                          <p class="story"></p> </div>
+                     <?php endwhile; ?>  
                   </div>
         </div>
     </section>

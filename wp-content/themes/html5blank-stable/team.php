770945df81f8420db28a37b1b8b33a91d59d2a4e
geekwise_veronica_chavez <veroch03@gmail.com>
2016-06-20 18:14:28 +0000

    bg color on team

--- wp-content/themes/html5blank-stable/team.php
+++ wp-content/themes/html5blank-stable/team.php
@@ -3,6 +3,7 @@
 <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 
 <?php get_header( 'team' ); ?>
+<main class="team_bg">
        <h3 class="team_text">Geekwise Academy was founded on the belief that we, as in the world, can do a whole lot better than we presently are in educating technologists. To accomplish that, we needed to assemble a team of people who know what they’re talking about. Here’s what we came up with.</h3>
          
             
@@ -19,3 +20,6 @@
        </div>
       <?php endwhile; ?>
    </section>
+   </main>
+   </body>
+   </html>
